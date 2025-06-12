cb067f607d3ac94b136f419fd13195f239959c2f
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-06-11 17:03:00 -0700

    React Native Components.
    Basic React Native component. (Text, StyleSheet, View)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { Text, StyleSheet, View } from 'react-native';
 
-export default function Index() {
+const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this weldone.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    color: 'green',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+});
+
+export default App;
+
+
+
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Edit app/index.tsx to edit this weldone.</Text>
+//     </View>
+//   );
+// }
