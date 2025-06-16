50302e067fe69f51dcb977920070c3693181899c
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-06-16 16:08:18 -0700

    Routing and Navigation (it works like next.js routing)

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -7,6 +8,9 @@ export default function Index() {
      className="flex-1 justify-center items-center"
     >
       <Text className="text-dark-200 text-5xl font-bold">Welcome</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/[id], query: { id: 'avengers' }">Movie Details</Link>
+     
     </View>
   );
 }
